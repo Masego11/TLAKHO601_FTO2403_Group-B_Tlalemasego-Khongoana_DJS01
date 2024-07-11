a9d8217ca8f4874ba656e6b1a7ff21efdf9a64ca
Tlalemasego <tlalemasego@gmail.com>
2024-07-11 12:18:33 +0200

    git: changed variable names to more descriptive ones.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Given Parameters
 const calculationValues = {
-  vel: 10000,  // velocity (km/h)
-  acc: 3, // acceleration (m/s^2)
+  velocity: 10000,  // velocity (km/h)
+  acceleration: 3, // acceleration (m/s^2)
   time: 3600, // seconds (1 hour)
-  d: 0, // distance (km)
+  distance: 0, // distance (km)
   fuel: 5000, // remaining fuel (kg)
-  fbr: 0.5, // fuel burn rate (kg/s)
+  fuelBurnRate: 0.5, // fuel burn rate (kg/s)
 
 }
-const calcNewVel = (vel, acc, time) => { //declared the calcNewVel and moved it above the calculation as it could not be accessed bofore initialization 
-  return vel + (acc * time * 3.6 ) //to convert (m/s to km)
+const calcNewVel = (velocity, acceleration, time) => { //declared the calcNewVel and moved it above the calculation as it could not be accessed bofore initialization 
+  return velocity + (acceleration * time * 3.6 ) //to convert (m/s to km)
 }
-const d2 = calculationValues.d + (calculationValues.vel * calculationValues.time / 3600) //calcultes new distance in kms
-const rf = calculationValues.fuel - (calculationValues.fbr * calculationValues.time )//calculates remaining fuel
-const vel2 = calcNewVel(calculationValues.vel, calculationValues.acc, calculationValues.time) //calculates new velocity based on acceleration
+const distance2 = calculationValues.distance + (calculationValues.velocity * calculationValues.time / 3600) //calcultes new distance in kms
+const remainingFuel = calculationValues.fuel - (calculationValues.fuelBurnRate * calculationValues.time )//calculates remaining fuel
+const velocity2 = calcNewVel(calculationValues.velocity, calculationValues.acceleration, calculationValues.time) //calculates new velocity based on acceleration
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${velocity2} km/h`);
+console.log(`Corrected New Distance: ${distance2} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
